db9b34345fc7a2a26c3d32b8bdfa6c7f51fa4718
EC2 Default User <se.eguti0824@gmail.com>
2022-10-12 02:58:30 +0000

    layout

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,8 +2,16 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     has_many :game_scores
-    
     has_many :favorites ,dependent: :destroy
+    
+    has_one_attached :customer_image
+    
+    def get_customer_image
+      unless image.attached?
+        画像がありません
+      end
+      image.variant(resize_to_limit: [200,200]).processed
+    end
       
          
   def active_for_authentication?
