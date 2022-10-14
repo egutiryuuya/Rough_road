d36ce460fa7633550bc18b84b19386b8c2fad664
EC2 Default User <se.eguti0824@gmail.com>
2022-10-14 03:05:50 +0000

    edit

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -10,14 +10,12 @@ class Public::FavoritesController < ApplicationController
     @game_score = GameScore.find(params[:game_score_id])
     favorite = current_customer.favorites.new(game_score_id: @game_score.id)
     favorite.save
-    redirect_to game_title_game_score_path(@game_score.game_title_id,@game_score)
   end
   
   def destroy
     @game_score = GameScore.find(params[:game_score_id])
     favorite = current_customer.favorites.find_by(game_score_id: @game_score.id)
     favorite.destroy
-    redirect_to game_title_game_score_path(@game_score.game_title_id,@game_score)
   end
   
 end
