a27482708176390934e398eb1c2fc8604902c076
EC2 Default User <se.eguti0824@gmail.com>
2022-10-22 06:42:38 +0000

    game title rb edit

--- app/models/game_title.rb
+++ app/models/game_title.rb
@@ -1,6 +1,7 @@
 class GameTitle < ApplicationRecord
   has_one_attached :image
   has_many :game_scores ,dependent: :destroy
+  validates :image, presence: true
   
   def get_game_title_image
       image.variant(resize_to_fill: [200, 200]).processed
