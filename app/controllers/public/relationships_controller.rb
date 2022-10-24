9c3fe32ac13beb5bce16e238a012cec3fdde3911
EC2 Default User <se.eguti0824@gmail.com>
2022-10-24 00:07:08 +0000

    add follow ajax

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -4,13 +4,13 @@ class Public::RelationshipsController < ApplicationController
   end
   
   def create
+    @customer = Customer.find(params[:customer_id])
     current_customer.follow(params[:customer_id])
-    redirect_to request.referer
   end
   
   def destroy
+    @customer = Customer.find(params[:customer_id])
     current_customer.unfollow(params[:customer_id])
-    redirect_to request.referer
   end
   
   def followings
