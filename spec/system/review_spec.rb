6599ef2697bca4a8bcaf16f2d30070e44649a808
EC2 Default User <se.eguti0824@gmail.com>
2022-11-14 07:02:08 +0000

    [add]chrome

--- spec/system/review_spec.rb
+++ spec/system/review_spec.rb
@@ -2,32 +2,31 @@
 
 require 'rails_helper'
 
-describe "ログインのテスト" do
-  before do 
-    @customer = FactoryBot.create(:customer)
-  end
-end
 
-describe "ログイン画面に遷移" do 
-  before do 
-    visit new_customer_session_path
+describe "root_pathへの遷移" do
+  context '表示の確認' do
+    before do 
+      visit root_path
+    end
+    it 'トップ画面(root_path)にゲーム上達のためのサイトですと表示されている' do
+      expect(page).to have_content 'ゲーム上達のためのサイトです'
+    end
   end
 end
 
 
 
 describe "レビュー投稿のテスト" do
-  let!(:list) {create(:review,title:'hoge',body:'body')}
+  let!(:review) {create(:review,title:'hoge',body:'body')}
   describe 'トップ画面のテスト(root_path)' do
-    before do 
-      visit root_path
-    end
     
-    context '表示の確認' do
-      it 'トップ画面(root_path)にゲーム上達のためのサイトですと表示されている' do
-        expect(page).to have_content 'ゲーム上達のためのサイトです'
-      end
+  end
+end
+
+describe "練習" do
+  context "足し算" do
+    it "3+3が六になる" do 
+      expect(3+3).to eq 6 
     end
-    
   end
 end
